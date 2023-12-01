2e828d8cb292c446e19c68c44fd1ce9f8d647edc
jomel <jomel@DESKTOP-COJOR96>
2023-12-01 08:39:27 -0600

    xd

--- src/Interfaz/Comienzo.java
+++ src/Interfaz/Comienzo.java
@@ -5,7 +5,8 @@ package Interfaz;
 public class Comienzo {
     
     public static void main(String[] args) {
-        
+        Iniciar inicio = new Iniciar();
+        inicio.setBounds(0,0,0,0);
     }
     
 }
